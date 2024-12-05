f4766054e4cf3379cf082954b2faf9d5146571fc
Erwan Leroux <erwan.leroux2096@gmail.com>
2024-12-05 11:44:40 +0100

    #Commit 7

--- mcarre.cpp
+++ mcarre.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void main()
 {
-	CCarre Carre1;
+	CCarre Carre[4];
 	int dec_hori;
 	int dec_vert;
 	int long_cote;
@@ -34,22 +34,33 @@ void main()
 	cout << "Veuillez selectionner dy: ";
 	cin >> dy;
 
-	Carre1.Setsx(dec_hori);
-	Carre1.Setsy(dec_vert);
-	Carre1.Setcote(long_cote);
+	Carre[0].Setsx(dec_hori);
+	Carre[0].Setsy(dec_vert);
+	Carre[0].Setcote(long_cote);
 	//Carre1.Afficher();
-	Carre1.Getsx();
-	Carre1.Getsy();
-	Carre1.Getcote();
-	cout <<"\n-La longueur horizontal choisi est : "<< Carre1.Getsx();
-	cout << "\n-La longueur vertical choisi est : " << Carre1.Getsy();
-	cout << "\n-La longueur du cote choisi est : " << Carre1.Getcote();
+	Carre[0].Getsx();
+	Carre[0].Getsy();
+	Carre[0].Getcote();
+	cout <<"\n-La longueur horizontal choisi est : "<< Carre[0].Getsx();
+	cout << "\n-La longueur vertical choisi est : " << Carre[0].Getsy();
+	cout << "\n-La longueur du cote choisi est : " << Carre[0].Getcote();
 	
-	Carre1.Deplacer(direction, saut);
-	cout <<"\n-Programme Deplacement" << "\nSx=" << Carre1.Getsx() << "\nSy=" << Carre1.Getsy() << "\nCote=" << Carre1.Getcote();
-	Carre1.Setsx(dec_hori);
-	Carre1.Setsy(dec_vert);
-	Carre1.Deplacer(dx, dy);
-	cout <<"\n-Programme Surcharge" << "\nSx=" << Carre1.Getsx() << "\nSy=" << Carre1.Getsy() << "\nCote=" << Carre1.Getcote();
+	Carre[0].Deplacer(direction, saut);
+	cout <<"\n-Programme Carre 1" << "\nSx=" << Carre[0].Getsx() << "\nSy=" << Carre[0].Getsy() << "\nCote=" << Carre[0].Getcote();
+	Carre[1].Setsx(dec_hori);
+	Carre[1].Setsy(dec_vert);
+	Carre[1].Setcote(long_cote);
+	Carre[1].Deplacer(dx, dy);
+	cout <<"\n-Programme Carre 2" << "\nSx=" << Carre[1].Getsx() << "\nSy=" << Carre[1].Getsy() << "\nCote=" << Carre[1].Getcote();
+	Carre[2].Setsx(dec_hori);
+	Carre[2].Setsy(dec_vert);
+	Carre[2].Setcote(long_cote);
+	Carre[2].Deplacer(dx*2, dy-3);
+	cout << "\n-Programme Carre 3" << "\nSx=" << Carre[2].Getsx() << "\nSy=" << Carre[2].Getsy() << "\nCote=" << Carre[2].Getcote();
+	Carre[3].Setsx(dec_hori);
+	Carre[3].Setsy(dec_vert);
+	Carre[3].Setcote(long_cote);
+	Carre[3].Deplacer('o', saut+3-8);
+	cout << "\n-Programme Carre 4" << "\nSx=" << Carre[3].Getsx() << "\nSy=" << Carre[3].Getsy() << "\nCote=" << Carre[3].Getcote();
 
 }
